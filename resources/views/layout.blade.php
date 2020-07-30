1b6c5babd388c649278f318f50b7cee11675bf04
sakagu <sakatoshi10416@gmail.com>
2020-07-31 00:03:10 +0900

    add

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,15 +4,33 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
+  <title>Reforme</title>
   <link rel="stylesheet" href="/css/reset.css">
   <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 <header>
-  <!-- <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav> -->
+  <div class="header_label">
+    <div class="header_left">
+      <div class=header_title>
+        <h1>Reforme</h1>
+      </div>
+      <div clas="header_subtitle">
+        <h2>〜自分だけのリフォーム〜</h2>
+      </div>
+    </div>
+    <div class="header_right">
+      <div class="header_user">
+        <p>ユーザー名</p>
+      </div>
+      <div class="header_login">
+        <p>ログイン</p>
+      </div>
+      <div class="header_post">
+        <p>投稿する</p>
+      </div>
+    </div>
+ </div>
 </header>
 <main>
   @yield('content')
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,31 @@
 @extends('layout')
 
 @section('content')
-<h1>テスト</h1>
+  <div class="main">
+
+    <div class="main_title">
+      <h2>あなただけのリフォームを教えてください</h2>
+    </div>
+    <div class="main_explanation">
+      <p>住宅リフォーム専用投稿サイトです</p>
+      <p>デザインやこだわりポイントを自慢してみましょう</p>
+      <p>リフォームをご検討の方は参考に！</p>
+      <p>業者紹介も掲載</p>
+    </div>
+
+    <div class="main_post"> 
+      <h3>投稿一覧</h3>
+      <div class="main_post_box">
+        <div class="post_box">
+          <p>写真</p>
+          <p>価格帯</p>
+          <p>エリア</p>
+          <p>業者</p>
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
 @endsection
